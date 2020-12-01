a1d4aaee37fae6876cdc8e0430284a3eec7da362
Pillai Manish <pillaimanish00@gmail.com>
2020-12-01 14:18:05 +0530

    Bugs Removed

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -47,7 +47,8 @@ class ClassController extends Controller
         // GENERATA ORDER ID
 
         $new_order  = Order::create(['user_id'=>$request->user()->id]);
-        $payment->prepare(['order' => $new_order->id,'user' => $request->user()->id,'mobile_number' => $request->user()->mobile,'email' => $request->user()->email, 'amount' => $package->price,'callback_url' => 'https://vams-fitness.herokuapp.com/packages']);
+        $orderid = intval($new_order->id) + 4000;
+        $payment->prepare(['order' => $orderid,'user' => $request->user()->id,'mobile_number' => $request->user()->mobile,'email' => $request->user()->email, 'amount' => $package->price,'callback_url' => 'https://vams-fitness.herokuapp.com/packages']);
         return $payment->receive();
     }
 
