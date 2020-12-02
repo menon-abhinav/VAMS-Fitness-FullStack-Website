a66c76b66ddf6a0cb371b166d7349140cba29c49
Pillai Manish <pillaimanish00@gmail.com>
2020-12-02 09:36:43 +0530

    Admin Panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
     public function __construct()
     {
         if (!Auth::check()) {
-            return redirect('/adminlogin');
+            Redirect::to('login')->send();
        }
 }
 
