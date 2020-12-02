5f49b5e95924c4ad073bb0efaa099148f89aa13f
Pillai Manish <pillaimanish00@gmail.com>
2020-12-02 11:27:40 +0530

    Admin Panel

--- app/Http/Controllers/AdminAccountController.php
+++ app/Http/Controllers/AdminAccountController.php
@@ -21,7 +21,7 @@ class AdminAccountController extends Controller
         if(Auth::attempt(['email'=>$email,'password'=>$password])){
             
                 if (Auth::user()->user_type == 1){
-                        return redirect()->route('admin');
+                        return redirect()->intended('admin');
                     }
 
                 else{
