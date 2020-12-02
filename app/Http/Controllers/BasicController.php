ffafd1078cf820bbdb5a86f167b612986aa428e5
Pillai Manish <pillaimanish00@gmail.com>
2020-12-02 19:07:35 +0530

    Frontend changes

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -44,7 +44,6 @@ class BasicController extends Controller
         $message    = 'Your message has been sent, you will get a reply in email soon';
         // Actually show a success message
         }
-        return redirect('/contact',compact('message'));
-
+        return redirect()->back()->with('message', $message);
     }
 }
